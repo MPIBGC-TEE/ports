e9bc06c6d042a02eb1b492d987a6604fb0b82091
Markus Müller <markus.mueller.1.g@googlemail.com>
2021-01-18 12:32:54 +0100

    updated server helper

--- src/ports/server_helpers.py
+++ src/ports/server_helpers.py
@@ -2,14 +2,15 @@ from IPython.core.display import display, HTML
 from subprocess import run
 
 def print_commands(cluster, local_port=8880):
-    hostname=str(run(['hostname'],capture_output=True).stdout,'utf8').strip()
-    remote_host = hostname
+    # this function relies on the appropriate entries in the ./ssh-config of the client
+    # matagorda-from-home
+    # antakya-from-home
+    hostname=str(run(['hostname'],capture_output=True).stdout,'utf8').strip().lower()
+    remote_ssh_host = f'{hostname}-from-home'
     remote_port = (cluster.dashboard_link.split('/')[2]).split(':')[1]
-    ssh_command = f'ssh -L {local_port}:localhost:{remote_port} {remote_host}' 
-    ssh_command_from_home = f'ssh -L {local_port}:localhost:{remote_port} {remote_host}_from_home'
-    print("ssh command from institute: "+ssh_command)
-    print("ssh command from home: "+ssh_command_from_home)
-    
+    ssh_command_from_home = f'ssh -L {local_port}:localhost:{remote_port} {remote_ssh_host}'
+    print("ssh command: "+ssh_command_from_home)
+
     url = f'http://localhost:{local_port}'
     print(f"""forwarded dashboard: {url}""")
     display(HTML(f"""forwarded dashboard: <a href={url}>{url}</a>"""))
